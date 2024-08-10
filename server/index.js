9c8d5e1c52aa6cb89a5b8dcb66a42c7af8f5e629
sanemi07 <130812101+sanemi07@users.noreply.github.com>
2024-08-10 19:37:40 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const verifyProof = require('../utils/verifyProof');
+const { keccak256 } = require('ethereum-cryptography/keccak');
 
 const port = 1225;
 
@@ -8,14 +9,14 @@ app.use(express.json());
 
 // TODO: hardcode a merkle root here representing the whole nice list
 // paste the hex string in here, without the 0x prefix
-const MERKLE_ROOT = '';
+const MERKLE_ROOT = '4cdf89e56895bbcabd3551d431b8ba74239d026cfac7af5fcb1c15934a4eaf34';
 
 app.post('/gift', (req, res) => {
-  // grab the parameters from the front-end here
-  const body = req.body;
+ const {name,proof}=req.body;
+  const lea=keccak256(Buffer.from(name)).toString('hex');
 
   // TODO: prove that a name is in the list 
-  const isInTheList = false;
+  const isInTheList = tree.verifyProof(lea,proof,MERKLE_ROOT);
   if(isInTheList) {
     res.send("You got a toy robot!");
   }
