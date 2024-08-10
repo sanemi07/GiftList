027269c14338754bb5b0f8eb931245f81bf7c203
sanemi07 <130812101+sanemi07@users.noreply.github.com>
2024-08-10 19:37:02 +0530

    Update index.js

--- client/index.js
+++ client/index.js
@@ -6,11 +6,19 @@ const serverUrl = 'http://localhost:1225';
 
 async function main() {
   // TODO: how do we prove to the server we're on the nice list? 
+  const tree=new MerkleTree(niceList);
+  const name ="Joel Raynor";
+  const root = tree.getRoot();
+  const index=niceList.findIndex(n => n === name);
+  const proof=tree.getProof(index);
 
   const { data: gift } = await axios.post(`${serverUrl}/gift`, {
     // TODO: add request body parameters here!
-  });
+    name:name,
+    proof:proof,
 
+  });
+console.log("sending to the server",{name,proof});
   console.log({ gift });
 }
 
